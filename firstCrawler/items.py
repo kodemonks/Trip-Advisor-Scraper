b328dfc1b94ed1c71e3cac4fcb9eaf94de680539
Bhupendra Singh <bhupendrasingh.suyal@gmail.com>
2017-03-03 02:44:58 +0530

    Updated items.py for items that we will store

--- firstCrawler/items.py
+++ firstCrawler/items.py
@@ -1,14 +1,10 @@
-# -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
-import scrapy
+from scrapy.item import Item,Field
 
 
-class FirstcrawlerItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+#Item Class to store Data
+class TripAdvisorItems(Item):
+    name=Field()
+    rating=Field()
+    latitude=Field()
+    longitude=Field()
+    url=Field()
